3f2e479f64263bebfb973d6f69da394349a401dc
MTahaB <113914857+MTahaB@users.noreply.github.com>
2024-05-17 11:23:15 +0200

    Add files via upload

---
+++ server/src/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  ethereumId: { type: String, required: true, unique: true }, 
+  balance: { type: Number, default: 0 },
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+export { userModel as User };
