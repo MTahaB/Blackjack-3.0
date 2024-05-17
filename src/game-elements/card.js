99ecf47d3852d62dd5bf3c3bdc7ab99fcb0c94c5
MTahaB <113914857+MTahaB@users.noreply.github.com>
2024-05-17 11:24:49 +0200

    Delete src directory

--- src/game-elements/card.js
+++
@@ -1,9 +0,0 @@
-export class Card {
-  constructor(suit, rank, value, id, isNew) {
-    this.suit = suit;
-    this.rank = rank;
-    this.value = value;
-    this.id = id;
-    this.isNew = isNew;
-  }
-}
